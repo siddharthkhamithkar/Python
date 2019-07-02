97e1f61a74c474e7af2cb81eac3fe5f6cf85d082
Siddharth Khamithkar <sidkhamithkar@gmail.com>
2019-07-02 16:51:51 +0530

    removed some embarassing stuff

---
+++ idkwhattonamethis.py
@@ -0,0 +1,12 @@
+#seriously I will be filling this with so many commments
+#also this will be completely random so yeah, your mistake if you happen to run across this
+print("This is completely random, so its your fault if you happen to run this script")
+name = raw_input("What must be your name? ")
+print("Oh I see... Your name is %s" % name)
+if name == "":
+    print("I need a name dude...")
+age = raw_input("Well, now that we have a name, what is your age? ")
+if age <= 10:
+    print("What are you even doing here? Your age is %s. That's young." % age)
+elif age >  10:
+    print("Well you seem to be jobless anyway.")
